61e13c32c858cbb8863f25a8507a4cdc2896cf45
yating <frontwebvueapp@163.com>
2023-09-28 09:52:08 +0800

    feat: create Test.vue file

---
+++ src/views/Test.vue
@@ -0,0 +1,12 @@
+<!-- -->
+<template>
+  <div>
+    {{  message }}
+  </div>
+</template>
+
+<script lang='ts' setup>
+  import { ref} from 'vue'
+  const message = ref('')
+</script>
+<style lang='scss' scoped></style>
